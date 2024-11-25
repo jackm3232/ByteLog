05f596f36281717126da9388631df0abcd9dffae
jackm3232 <jackmckee32@gmail.com>
2024-11-25 12:58:22 -0500

    JWT Auth logic implemented into backend

--- Backend/routes/itemTemplateRoutes.js
+++ Backend/routes/itemTemplateRoutes.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const itemTemplatesController = require("../controllers/itemTemplatesController");
+const verifyJWT = require("../middleware/verifyJWT");
+
+router.use(verifyJWT);
+
+router.route("/:user")
+  .get(itemTemplatesController.getAllItemTemplates);
 
 router.route("/")
-  .get(itemTemplatesController.getAllItemTemplates)
   .post(itemTemplatesController.createItemTemplate)
   .patch(itemTemplatesController.updateItemTemplate)
   .delete(itemTemplatesController.deleteItemTemplate);
--- Backend/server.js
+++ Backend/server.js
@@ -24,6 +24,7 @@ app.use(cookieParser());
 app.use("/", express.static(path.join(__dirname, "public")));
 
 app.use("/", require("./routes/root"));
+app.use("/auth", require("./routes/authRoutes"));
 app.use("/users", require("./routes/userRoutes"));
 app.use("/itemtemplates", require("./routes/itemTemplateRoutes"));
 app.use("/dailylogs", require("./routes/dailyLogRoutes"));
