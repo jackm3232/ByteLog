76ba1d7391c6d6bef75043faace73edfbe19495a
jackm3232 <jackmckee32@gmail.com>
2024-11-29 16:59:48 -0500

    Activate JWT Auth in backend

--- Backend/routes/dailyLogRoutes.js
+++ Backend/routes/dailyLogRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const dailyLogsController = require("../controllers/dailyLogsController");
 const verifyJWT = require("../middleware/verifyJWT");
 
-//router.use(verifyJWT);
+router.use(verifyJWT);
 
 router.route("/:user")
   .get(dailyLogsController.getAllDailyLogs)
